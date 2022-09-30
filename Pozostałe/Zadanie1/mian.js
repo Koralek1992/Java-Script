d3eedad6ff7326b98921660c1244a34bdb449de3
Koralek <dkoralek2@gmail.com>
2022-09-30 14:48:21 +0200

    Zadanie2

---
+++ Pozostałe/Zadanie1/mian.js
@@ -0,0 +1,9 @@
+const img = document.querySelector('img')
+
+img.setAttribute('src', 'https://unsplash.com/photos/aNhumRawzl8')
+
+const test = () => {
+    console.log('cokolwiek');
+}
+
+setTimeout(test, 2000)
